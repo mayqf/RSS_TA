7faef69723e70212c7d23a80f12380ecf4ea755b
mayqf <mayqf@mail.missouri.edu>
2020-05-26 12:37:53 +0200

    for deploy7

--- rssreader/settings.py
+++ rssreader/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://obscure-retreat-15024.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['obscure-retreat-15024.herokuapp.com','127.0.0.1']
 
 
 # Application definition
