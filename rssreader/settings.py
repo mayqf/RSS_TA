0aa0bf4ef238ec9c743ea379562bca1e81b12ead
mayqf <mayqf@mail.missouri.edu>
2020-05-26 12:18:34 +0200

    for deploy4

--- rssreader/settings.py
+++ rssreader/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/' 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'rss/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'rssreader/static')]
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
